94fe121650b13b583bac20df230335d11136e776
Nikolai Gruzinov <ngruzinov@constructor.university>
2024-01-20 23:34:38 +0100

    examples/main.cpp: deduce input/output shapes and names from .onnx file

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,6 +6,7 @@
 #include <array>
 #include <vector>
 #include <numeric>
+#include <optional>
 
 void printOutputToFile(const auto& data, const std::string& file_path) {
     std::ofstream output_file(file_path);
@@ -19,6 +20,44 @@ void printOutputToFile(const auto& data, const std::string& file_path) {
     }
 }
 
+/* Return nullopt if model unexpectedly has more than 1 input */
+std::optional<std::string> getInputName(const Ort::Session& session) {
+    const auto inputCount = session.GetInputCount();
+    if (inputCount != 1) {
+        return std::nullopt;
+    }
+    const auto namePtr = session.GetInputNameAllocated(0, Ort::AllocatorWithDefaultOptions());
+    return std::string(namePtr.get());
+}
+
+/* Return nullopt if model unexpectedly has more than 1 output */
+std::optional<std::string> getOutputName(const Ort::Session& session) {
+    const auto outputCount = session.GetOutputCount();
+    if (outputCount != 1) {
+        return std::nullopt;
+    }
+    const auto namePtr = session.GetOutputNameAllocated(0, Ort::AllocatorWithDefaultOptions());
+    return std::string(namePtr.get());
+}
+
+/* Return nullopt if model unexpectedly has more than 1 input */
+std::optional<const std::vector<int64_t>> getInputShape(const Ort::Session& session) {
+    const auto inputCount = session.GetInputCount();
+    if (inputCount != 1) {
+        return std::nullopt;
+    }
+    return session.GetInputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+}
+
+/* Return nullopt if model unexpectedly has more than 1 output */
+std::optional<const std::vector<int64_t>> getOutputShape(const Ort::Session& session) {
+    const auto outputCount = session.GetOutputCount();
+    if (outputCount != 1) {
+        return std::nullopt;
+    }
+    return session.GetOutputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+}
+
 int main() {
     Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ONNXRuntimeModel");
 
@@ -28,26 +67,26 @@ int main() {
 
     auto memoryInfo = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
 
-    constexpr auto inputShape = std::array<int64_t, 4>{1, 3, 936, 662};
-    const char* const inputNames[] = {"input.1"};
-    constexpr int64_t inputSize = std::accumulate(inputShape.begin(), inputShape.end(), 1, std::multiplies<int64_t>());
+    const auto singleInputName = getInputName(session).value();
+    const char* const inputNames[] = {singleInputName.c_str()};
+    const auto inputShape = getInputShape(session).value();
+    const int64_t inputSize = std::accumulate(inputShape.begin(), inputShape.end(), 1, std::multiplies<int64_t>());
     std::vector<float> rawInput(inputSize, 0);
     Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
         memoryInfo, rawInput.data(), rawInput.size(), inputShape.data(), inputShape.size()
     );
 
-    constexpr auto outputShape = std::array<int64_t, 4>{1, 1, 936, 662};
-    constexpr int64_t outputSize = std::accumulate(outputShape.begin(), outputShape.end(), 1, std::multiplies<int64_t>());
+    const auto singleOutputName = getOutputName(session).value();
+    const char* const outputNames[] = {singleOutputName.c_str()};
+    const auto outputShape = getOutputShape(session).value();
+    const int64_t outputSize = std::accumulate(outputShape.begin(), outputShape.end(), 1, std::multiplies<int64_t>());
     std::vector<float> rawOutput(outputSize, 0);
-    const char* const outputNames[] = {"293"};
     Ort::Value outputTensor = Ort::Value::CreateTensor<float>(
         memoryInfo, rawOutput.data(), rawOutput.size(), outputShape.data(), outputShape.size()
     );
 
-    constexpr size_t inputCount = 1;
-    constexpr size_t outputCount = 1;
-    session.Run(Ort::RunOptions{}, inputNames, &inputTensor, inputCount,
-                             outputNames, &outputTensor, outputCount);
+    session.Run(Ort::RunOptions{}, inputNames, &inputTensor, 1,
+                             outputNames, &outputTensor, 1);
     
     // Write tensor to disk, then examine the output visually in Python, for simplicity
     printOutputToFile(rawOutput, "onnx_model_raw_output.txt");
